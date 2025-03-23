b31b6858f87ed9a7a82f5966a1a20d43a33f7157
smileleft <spyt77@gmail.com>
2025-03-23 13:57:41 +0900

    Update model.py
    create item

--- model.py
+++ model.py
@@ -7,3 +7,21 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
+
+
+# schemas.py
+# --------------------
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    name: str
+    description: str
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
